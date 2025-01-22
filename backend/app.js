c6f9c567d09593a37314e87272b89683cff09bce
Victor <victorjost34@gmail.com>
2025-01-22 12:52:12 -0500

    Ajout server et la base de app avec setup de mongoose

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const path = require('path');
+
+//const stuffRouter = require('./routes/stuff')
+//const userRouter = require('./routes/user')
+
+mongoose.connect('mongodb+srv://api:wWk7QFVWsMiAzbF7@cluster0.jsudq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée :' + error));
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  }
+);
+
+/*
+app.use('/api/stuff', stuffRouter)
+app.use('/api/auth', userRouter)
+app.use('/images', express.static(path.join(__dirname, 'images')))
+*/
+module.exports = app
