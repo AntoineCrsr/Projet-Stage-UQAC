886eb8122eee509748869ff5b0d95ec5969492a5
Victor Jost <96876377+Zarcoks@users.noreply.github.com>
2025-02-12 15:23:50 -0500

    MAJ format données user

--- backend/components/users/userModel.js
+++ backend/components/users/userModel.js
@@ -3,35 +3,57 @@ const validator = require('mongoose-unique-validator')
 
 const user = mongoose.Schema({
     email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     isStudent: {type: Boolean, required: true},
     dateBirthday: {type: Date, required: true}, // type ?
-    phoneType: {type: String, required: true}, // type object ? interface ? (type: mobile, work, pager, other) + prefix (+1, +33...)
-    phonePrefix: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
-    publicName: {type: String, required: true},
-    showAgePublically: {type: Boolean, required: true},
-    showEmailPublically: {type: Boolean, required: true},
-    showPhonePublically: {type: Boolean, required: true},
-    sendNewsletter: {type: Boolean, required: true},
-    remindEvaluations: {type: Boolean, required: true}, // Envoie un mail pour rappeler d'évaluer un trajet qu'on a eu
-    remindDeparture: {type: Boolean, required: true}, // Envoie un mail pour vérifier l'identité de nos passagers avant d'embarquer
-    preferredLangage: {type: String, required: true},
-    nbRidesCompleted: {type: Number, required: true},
-    nbKmTravelled: {type: Number, required: true},
-    nbPeopleTravelledWith: {type: Number, required: true},
-    nbTonsOfCO2Saved: {type: Number, required: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true, unique: false},
-    lastName: {type: String, required: true, unique: false},
-    // Not required:
     aboutMe: {type: String, required: false},
     alternameEmail: {type: String, required: false},
     testimonial: {type: String, required: false},
     nonce: {type: String, required: false},
-    punctualityRating: {type: Number, required: false},
-    securityRating: {type: Number, required: false},
-    comfortRating: {type: Number, required: false},
-    courtesyRating: {type: Number, required: false},
+
+    name: {
+        publicName: {type: String, required: true},    
+        firstName: {type: String, required: true, unique: false},
+        lastName: {type: String, required: true, unique: false},
+    },
+
+    phone: {
+        type: {
+            type: String, required: true, enum: ["mobile", "work", "pager", "other"]
+        },
+        prefix: { type: String, required: true, enum: ["+1", "+33", "+44", "+49", "+91"] },
+        number: { type: String, required: true },
+        phoneExt: {type: String, required: false},
+        phoneDescription: {type: String, required: false},
+    },
+
+    rating: {
+        punctualityRating: {type: Number, required: false},
+        securityRating: {type: Number, required: false},
+        comfortRating: {type: Number, required: false},
+        courtesyRating: {type: Number, required: false},
+    },
+
+    parameters: {
+        show: {
+            showAgePublically: {type: Boolean, required: true},
+            showEmailPublically: {type: Boolean, required: true},
+            showPhonePublically: {type: Boolean, required: true},
+        },
+        notification: {
+            sendNewsletter: {type: Boolean, required: true},
+            remindEvaluations: {type: Boolean, required: true}, // Envoie un mail pour rappeler d'évaluer un trajet qu'on a eu
+            remindDeparture: {type: Boolean, required: true}, // Envoie un mail pour vérifier l'identité de nos passagers avant d'embarquer
+        },
+        preferredLangage: {type: String, required: true}
+    },
+
+    statistics: {
+        nbRidesCompleted: {type: Number, required: true},
+        nbKmTravelled: {type: Number, required: true},
+        nbPeopleTravelledWith: {type: Number, required: true},
+        nbTonsOfCO2Saved: {type: Number, required: true},
+    },
 })
 
 // Gestion du code de vérification nonce pour téléphone ?
