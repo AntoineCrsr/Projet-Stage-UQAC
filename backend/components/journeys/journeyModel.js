d79c4a67eec3daf0341a44d0ccbc8191ecba2905
Victor Jost <96876377+Zarcoks@users.noreply.github.com>
2025-02-12 19:03:14 -0500

    Journey model v1

---
+++ backend/components/journeys/journeyModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const validator = require('mongoose-unique-validator')
+
+const journey = mongoose.Schema({
+    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    starting: {
+        city: { type: String, required: true },
+        adress: { type: String, required: true }
+    },
+    arrival: {
+        city: { type: String, required: true },
+        adress: { type: String, required: true }
+    },
+    date: { type: Date, required: true },
+    seats: {
+        total: { type: Number, required: true },
+        left: { type: Number, required: true },
+    },
+    price: { type: Number, required: true },
+    passengers: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+    ]
+    // Maybe add payment type one day
+    // Also considering the preferences such as smoking, animals etc.
+    // Maybe also driver license ?
+})
+
+journey.plugin(validator)
+
+module.exports = mongoose.model("Journey", journey)
