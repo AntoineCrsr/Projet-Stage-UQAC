2caafcbd0fdd2945c83db48ebe411c6a97a3957f
AntoineCrsr <antoinecrauser@hotmail.com>
2025-03-02 13:44:52 -0500

    nettoyage de code commenté

--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -1,27 +1,3 @@
-/* import { useState } from "react";
-import Header from "../components/header";
-import LoginForm from "../components/loginform";
-
-
-const Home = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <Header />
-      <button
-        className="mt-10 bg-blue-500 text-white px-6 py-3 rounded-lg"
-        onClick={() => setShowLogin(true)}
-      >
-        Se connecter
-      </button>
-      {showLogin && <LoginForm onClose={() => setShowLogin(false)} />}
-    </div>
-  );
-};
-
-export default Home; */
-
 import React from "react";
 import Header from "../components/header";
 import "./styles/home.css";
