d3b096e871b3baa3cb064381f5c6eade2ab5d1e2
AntoineCrsr <antoinecrauser@hotmail.com>
2025-04-30 15:13:23 -0400

    MAJ de LoginCompletion, formulaire d'inscription terminé.

--- frontend-react/src/pages/LoginCompletion.jsx
+++ frontend-react/src/pages/LoginCompletion.jsx
@@ -8,6 +8,11 @@ const LoginCompletion = () => {
     const [gender, setGender] = useState("");
     const [birthDate, setBirthDate] = useState("");
     const [error, setError] = useState("");
+
+    const [phonePrefix, setPhonePrefix] = useState("+1");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [phoneType, setPhoneType] = useState("mobile");
+
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -19,11 +24,17 @@ const LoginCompletion = () => {
         return;
     }
 
-    /*const token = localStorage.getItem("token");
-    const userId = localStorage.getItem("userId");*/
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    console.log("USER ID:", userId);
+    if (!token || !userId) {
+      setError("Informations utilisateur manquantes. Veuillez vous reconnecter.");
+      return;
+    }
 
-    /*try {
-        const res = await fetch(`http://localhost:3000/api/user/${userId}`, {
+
+    try {
+        const res = await fetch(`http://localhost:3000/api/auth/${userId}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
@@ -31,25 +42,46 @@ const LoginCompletion = () => {
         },
         body: JSON.stringify({
             user: {
-            name: {
-                firstName,
-                lastName,
-            },
+              name: {
+                  firstName,
+                  lastName,
+              },
             gender,
-            dateBirthday: new Date(birthDate).getTime(),
-            },
+            dateBirthday: new Date(birthDate).toISOString(),
+            phone: {
+              type: phoneType,
+              prefix: phonePrefix,
+              number: phoneNumber,
+            }
+          },
         }),
     });
+      console.log(res)
+      if (res.status === 400) {
+        setError("Champs invalides");
+        return;
+      }
 
-      if (!res.ok) throw new Error("Erreur API");
+      if (res.status === 401) {
+        setError("Non autorisé. Veuillez vous reconnecter");
+        return;
+      }
 
+      if (res.status === 409) {
+        setError("téléphone déjà utilisé");
+        return;
+      }
+
+      if (!res.ok) {
+        throw new Error("Erreur lors de la mise à jour");
+      }
       // MAJ nom localstorage
       localStorage.setItem("userName", `${firstName} ${lastName}`);
       navigate("/"); // retour à l'accueil une fois le formulaire complété (pas encore testé)
     } catch (err) {
       console.error(err);
-      setError("Impossible de compléter le profil. Réessayez.");
-    }*/
+      setError("Erreur lors de l'envoi des données");
+    }
   };
 
   return (
@@ -82,6 +114,35 @@ const LoginCompletion = () => {
             <input type="date" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} />
           </div>
 
+          <div className="input-group">
+
+            <label>Numéro de téléphone</label>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <select
+                value={phonePrefix}
+                onChange={(e) => setPhonePrefix(e.target.value)}
+              >
+                <option value="+1">+1 (CAN/US)</option>
+                <option value="+33">+33 (FR)</option>
+              </select>
+
+              <input
+                type="tel"
+                placeholder="Numéro"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+              
+              <select
+                value={phoneType}
+                onChange={(e) => setPhoneType(e.target.value)}
+              >
+                <option value="mobile">Mobile</option>
+                <option value="fixe">Fixe</option>
+                <option value="pro">Pro</option>
+              </select>
+            </div>
+          </div>
           <button className="form" type="submit">Valider</button>
         </form>
 
