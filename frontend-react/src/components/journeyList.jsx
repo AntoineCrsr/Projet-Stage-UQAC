5fffc366fa934a091502d9a2e8dc9fd92be1a909
AntoineCrsr <antoinecrauser@hotmail.com>
2025-05-07 11:39:40 -0400

    Modif du composant pour indiquer les trajets créés par l'user conneccté, ceux qui sont terminés, ceux qui sont complets, et ceux qui sont réservés par l'user connecté

--- frontend-react/src/components/journeyList.jsx
+++ frontend-react/src/components/journeyList.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 
 const JourneyList = () => {
   const [journeys, setJourneys] = useState([]);
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+  const [reservedJourneyIds, setReservedJourneyIds] = useState([]);
   
   useEffect(() => {
     fetch("http://localhost:3000/api/journey")
@@ -17,6 +20,21 @@ const JourneyList = () => {
       })
       .catch((err) => console.error("Erreur lors du chargement des trajets :", err));
   }, []);
+
+  //recuperer les reservations faites par l'user connecter pour lui indiquer dans le composant
+  useEffect(() => {
+    if (token && userId) {
+      fetch(`http://localhost:3000/api/reservation?userId=${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const ids = data.map((res) => res.journeyId);
+          setReservedJourneyIds(ids);
+        })
+        .catch((err) => console.error("Erreur réservations :", err));
+    }
+  }, [token, userId]);
 
   return (
     <div className="journey-list-wrapper">
@@ -45,17 +63,39 @@ const JourneyList = () => {
                 minute: "2-digit"
               }).replace(",", ""); // enlève la virgule
               //attention j'ai une inversion de logique sur le calcul des places libres, à revoir.
+              const isPast = new Date(journey.date) < new Date();
+              const isFull = journey.seats.left === 0;
+              const isCreator = journey.ownerId === userId;
+              const isReserved = reservedJourneyIds.includes(journey._id);
+
+              const disabled = isPast || isFull;
+              const rowClass = disabled ? "disabled-row" : isCreator ? "creator-row" : "";
+              
               return (
-                <tr key={journey._id}>
+                <tr key={journey._id} className={rowClass}>
                   <td>{journey.starting.city}</td>
                   <td>{journey.arrival.city}</td>
                   <td>{formatDate}</td>
                   <td>{journey.seats.total - journey.seats.left}/{journey.seats.total}</td> 
                   <td>{journey.price} $ CAD</td>
                   <td>
+                    {isCreator ? (
                     <Link to={`/journey/${journey._id}`} className="btn-details-liste">
-                      details/reserver
-                    </Link>
+                        Détails de votre trajet
+                      </Link>
+                    ) : isReserved ? (
+                      <span className="badge-reserved">Déjà réservé</span>
+                    ) : isFull ? (
+                      <span className="badge-reserved">Trajet complet</span>
+                    ) : isPast ? (
+                      <span className="badge-reserved">Trajet déjà terminé</span>
+                    ) : disabled ? (
+                      <span className="btn-disabled">Indisponible</span>
+                    ) : (
+                      <Link to={`/journey/${journey._id}`} className="btn-details-liste">
+                        détails/réserver
+                      </Link>
+                    )}
                   </td>
                 </tr>
               );
