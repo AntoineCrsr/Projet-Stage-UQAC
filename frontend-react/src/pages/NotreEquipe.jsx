43cdf2d3ea6a1eca8ef894ca43f204b6dea8ef72
AntoineCrsr <antoinecrauser@hotmail.com>
2025-05-12 22:29:45 -0400

    Page NotreEquipe

--- frontend-react/src/pages/NotreEquipe.jsx
+++ frontend-react/src/pages/NotreEquipe.jsx
@@ -7,11 +7,15 @@ const NotreEquipe = () => {
         <div className="notre-equipe">
         <h1>Notre Equipe pour ce projet</h1>
         <p>
-            Sur ce projet ...
+            Sur ce projet, nous sommes seulement 2 étudiants à travailler et découvrons au fûr et à mesure du projet les technologies 
+            qu'on utilise.
+        </p>
+
+        <p>
+            Vous pourrez trouver dans le footer des logos qui vous permettrons de nous retrouver sur LinkedIn et sur Github.
         </p>
         </div>
     );
-
 };
 
 export default NotreEquipe;
