04233657c0da0c989a510b6dbdc8899c59664d41
AntoineCrsr <antoinecrauser@hotmail.com>
2025-05-12 22:29:27 -0400

    Page APropos

--- frontend-react/src/pages/APropos.jsx
+++ frontend-react/src/pages/APropos.jsx
@@ -6,10 +6,22 @@ const APropos = () => {
         <div className="a-propos">
         <h1>À propos de Travel Express</h1>
         <p>
-            Travel Express est ...
+            Travel Express est un projet de stage étudiant qui nous permet de découvrir le développement d'application web complet avec Backend 
+            et FrontEnd. <br />
+
         </p>
         <p>
-            Ce projet a été conçu ...
+            Ce projet a été conçu dans un contexte de stage et d’augmentation constante de la pollution et des manques de moyens de transports 
+            écologiques, le développement d’une application web de covoiturage apparaît comme une solution essentielle. 
+            <br /><br />
+            En effet, avec l’augmentation des préoccupations liées à l’empreinte écologique, les initiatives favorisant le partage des trajets 
+            sont devenues une priorité.  Au-delà de l’écologie et de l’économie d’argent, la problématique de la confiance est aussi au centre 
+            des enjeux. <br />
+
+            <br />
+            Ce projet a donc facticement pour ambition de connecter les utilisateurs en temps réel, de simplifier l’organisation des déplacements, 
+            et de promouvoir une alternative économique et écologique aux modes de transport traditionnels au Québec tout en assurant leur sécurité.
+
         </p>
         </div>
     );
